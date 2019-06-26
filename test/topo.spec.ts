a86339a19322723bbd81ece91b774c1a49c93df9
paul.rutledge <paul.rutledge@vodori.com>
2019-06-26 15:57:07 -0500

    cleanup

--- test/topo.spec.ts
+++ test/topo.spec.ts
@@ -3,31 +3,6 @@ import {deepEquals} from "../src/utils";
 
 test('shortest path', () => {
 
-    const g = {};
-
-    for (let i = 0; i < 6; i++) {
-
-        g[i + ''] = [];
-
-        for (let j = 0; j < 6; j++) {
-            if (i !== j) {
-
-                g[j + ''] = g[j + ''] || [];
-
-                if (g[i + ''].indexOf(j + '') === -1) {
-                    g[i + ''].push(j + '');
-                }
-
-                if (g[j + ''].indexOf(i + '') === -1) {
-                    g[j + ''].push(i + '');
-                }
-            }
-        }
-    }
-
-    // A B C
-    // D E F
-    // G H I
     const graph = {
         a: ['b', 'd'],
         b: ['a', 'c', 'e'],
@@ -40,9 +15,7 @@ test('shortest path', () => {
         i: ['f', 'h']
     };
 
-    console.log(g);
-
-    console.log(shortestPath(graph, 'a', 'i'));
+    expect(shortestPath(graph, 'a', 'i')).toEqual([ 'a', 'b', 'c', 'f', 'i' ]);
 
 });
 
