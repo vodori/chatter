9ba65055a421336ceb0492a96134bd463b2570c6
Sasha Klymenko <sasha.klymenko@vodori.com>
2022-11-13 23:31:51 +0200

    GHOST-9831 Return sefl as global object for service worker part

--- src/models.ts
+++ src/models.ts
@@ -4,13 +4,13 @@ function getGlobal(): any {
     try {
         return window === undefined ? {} : window;
     } catch (error) {
-        return {};
+        return self;
     }
 };
 
 export const _global = getGlobal();
 
-export const _chrome = _global.chrome;
+export const _chrome = chrome || _global.chrome;
 export const _window: Window = _global.window;
 export const _document: Document = _global.document;
 export const _localMessageBus = new Subject<NetPacket>();
